418094327a24a20872cddc79bfb9debc6b54fff2
CastilloValeria <casvaleli@gmail.com>
2023-12-27 22:57:23 -0300

    subiendo cambios producto de aplicar  multer  y arregla modificarr el controller

--- ElectroGropu/src/controllers/productControllers.js
+++ ElectroGropu/src/controllers/productControllers.js
@@ -20,7 +20,17 @@ const detailcontrollers = {
     res.render('products/dashboard', { title: "Dashboard", products });
     },
     formCreate:(req, res) => {
-    res.render('products/createProduct', { title: "Create Product" });
+        res.render('products/createProduct', { title: "Create Product" });
+    },
+    formEdit: (req,res)=>{
+        const file = req.file;
+        console.log(file);
+        const {id}= req.params;
+        const json = fs.readFileSync(path.join(__dirname,"../data/products.json"),"utf-8");
+        const products = JSON.parse(json);
+        const product= products.find(product => product.id == id);
+        console.log(product);
+        res.render("products/editProduct", {title:products.titulo,product});
     },
 
 create:(req, res) => {
@@ -32,50 +42,50 @@ create:(req, res) => {
     res.redirect("/products/dashboard");
 },
 
-formUpdate: (req, res) => {
-    const {id} = req.params;
-    const product = products.find(producto => producto.id == id);
-    res.render('products/createProduct', { title: product.nombre, product });
-},
-update: (req, res) => {
-    const {id} = req.params;
-    const product = products.find(producto => producto.id == id);
-    res.redirect("/products/dashboard");
-},
+// formUpdate: (req, res) => {
+    // const {id} = req.params;
+    // const product = products.find(producto => producto.id == id);
+    // res.render('products/createProduct', { title: product.nombre, product });
+// },
+// update: (req, res) => {
+    // const {id} = req.params;
+    // const product = products.find(producto => producto.id == id);
+    // res.redirect("/products/dashboard");
+// },
 productDelete: (req, res) => {
     const {id} = req.params;
     const product = products.find(producto => producto.id == id);
     res.redirect("/products/dashboard");
 },
 editProduct: (req, res) => {
-    const { id } = req.params;
-    const product = products.find(product => product.id == id);
+    const {id} = req.params;
     const { titulo, description, image, price} = req.body;
+    console.log(req.body);
+    const json = fs.readFileSync(path.join(__dirname,"../data/products.json"),"utf-8")
+    const products = JSON.parse(json);
 
     const nuevoArray = products.map(product => {
         if (product.id == id) {
             return {
                 id,
                 titulo: titulo,
-                descripcion: description,
-                precio: +price,
-                imagen: image ? image : product.imagen,
+                description: description,
+                price: +price,
+                image: image ? image : product.image,
             };
         }
         return product;
     });
 
-    products.forEach((product, index) => {
-        if (product.id == id) {
-            products[index] = nuevoArray.find(produc => product.id == id);
-        }
-    });
-
-    const jsonData = JSON.stringify(products);
-    fs.writeFileSync(path.join(__dirname, "../data/products.json"), jsonData);
-    res.render("products/editProduct", { title: product.nombre, product });
-    res.redirect(`/products/dashboad/${product.id}`);
+    // products.forEach((product, index) => {
+        // if (product.id == id) {
+            // products[index] = nuevoArray.find(product => product.id == id);
+        // }
+    // });
+    const jsonData = JSON.stringify(nuevoArray);
+    fs.writeFileSync(path.join(__dirname, "../data/products.json"), jsonData,"utf-8");
+    res.redirect(`/products/dashboard`);
 },
-
 }
+
 module.exports = detailcontrollers;
--- ElectroGropu/src/routes/products.js
+++ ElectroGropu/src/routes/products.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
-const {productDetail, productCart,formCreate,dashboard,create,formUpdate,update,productDelete,editProduct} = require("../controllers/productControllers")
+const multer = require('multer');
+const {productDetail, productCart,formCreate,dashboard,create,formUpdate,update,productDelete,editProduct,formEdit} = require("../controllers/productControllers")
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+    cb(null,path.join(__dirname,'../../public/img/articulos'))
+    },
+    filename: (req,file,cb) =>{
+    let newFile = "product-"+ Date.now()+ path.extname(file.originalname);
+    cb(null,newFile)
+    }
+})
+
+const upLoad = multer({storage});
 
 router
 .get('/productDetail/:id', productDetail)
@@ -8,9 +22,12 @@ router
 .get('/dashboard', dashboard)
 .get('/createProduct', formCreate)
 .post('/create', create)
-.get('/formUpdate/:id',formUpdate)
-.put('/update/:id', update)
+// .get('/formUpdate/:id',formUpdate)
+// .put('/update/:id', update)
 .delete('/delete/:id', productDelete)
-.get('/editProduct/:id',editProduct)
-.put('/editProduct/:id',editProduct)
+.get('/editProduct/:id',formEdit)
+.put('/editProduct/:id', upLoad.array('imagenProducto'),editProduct)
+
+
+
 module.exports = router
