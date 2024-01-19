17215c41af584617c2155cd5d57f032ae91e4471
CastilloValeria <casvaleli@gmail.com>
2024-01-19 09:27:42 -0300

    modificacion en el formulario de registre.ejs y agregando archivos con validaciones

--- ElectroGropu/src/routes/users.js
+++ ElectroGropu/src/routes/users.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {login,register,createUser} = require("../controllers/userControllers");
+const upload = require('../validaciones/validationAvatarUser');
+const validationRegister = require("../validaciones/validationRegister");
+
+
 /* GET users listing. */
 router.get('/login', login);
-router.get('/register', register);
-router.post('/register', createUser);
+router.get('/register',validationRegister ,register);
+router.post('/register',upload.single('avatar') ,ValidationAvatar ,createUser);
 
 module.exports = router;
