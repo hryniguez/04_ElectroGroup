b4a198c093821f9ea86aeaf717c5881fe4110fd2
mayco91 <mayconitido@gmail.com>
2024-02-20 21:22:27 -0300

    creacion de migraciones y modelos con sus asociaciones

---
+++ ElectroGropu/src/database/models/User.js
@@ -0,0 +1,74 @@
+
+module.exports = (sequelize, DataTypes) => {
+  let alias = "User";
+  let cols={
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull:false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull:false
+  },
+  birthday: {
+    type: DataTypes.DATE,
+    allowNull:false
+  },
+  genre: {
+    type: DataTypes.STRING,
+    allowNull:false
+  },
+  rol_id: {
+    type: DataTypes.INTEGER
+  },
+  avatar: {
+    type: DataTypes.STRING
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE
+  },
+  updatedAt: {
+    allowNull: false,
+    type: DataTypes.DATE
+  }
+};
+let config = {
+  tableName: "users",
+  timestamps: true
+}
+
+const User = sequelize.define(alias,cols,config);
+
+User.associate = models=>{
+  User.hasMany(models.Adress,{
+    as:"adress",
+    foreignKey:"user_id"
+  })
+  User.hasMany(models.Contact,{
+    as:"contact",
+    foreignKey:"user_id"
+  })
+ 
+  User.belongsTo(models.Rol,{
+      as:"rol",
+      foreignKey:"user_id"
+  })
+
+  User.belongsToMany(models.Product,{
+    as:"product",
+    through:"cart",
+    foreignKey: "user_id",
+    otherKey:"product_id",
+    timestamps:true
+  })
+  
+}
+  return User;
+};
