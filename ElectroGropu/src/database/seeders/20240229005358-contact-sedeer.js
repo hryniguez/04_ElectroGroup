7524908ff2002715d90e6bcdaba1ac036f8bdae7
CastilloValeria <casvaleli@gmail.com>
2024-02-28 22:12:13 -0300

    haciendo seeders

---
+++ ElectroGropu/src/database/seeders/20240229005358-contact-sedeer.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  
+  await queryInterface.bulkInsert('contacts', [{
+    phone: 1523678145,
+    user_id: " ",
+    opcional_number:1123456789,
+    createdAt:new Date,
+    updatedAt: new Date
+    }], {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+  
+    await queryInterface.bulkDelete('contacts', null, {});
+  
+  }
+};
